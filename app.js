1a770629867f717fb755d510df0f6f3bf301657c
saaaaadraja <rmsik92@gmail.com>
2023-09-25 16:52:01 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,5 @@ const app = express();
 app.listen(3000);
 
 app.get('/',(req,res)=>{
-    res.json({
-        msg:'Hello World...'
-    })
+    res.send(<h2 style="width:100%;align-text:center;">This is my test express.js app....</h2>)
 })
