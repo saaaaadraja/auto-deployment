4b0e9aaac9388e34e19a8b9468c51c1e01a6e04e
rajasaad <rmsik92@gmail.com>
2023-09-25 19:06:35 +0500

    changes

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ const app = express();
 app.listen(3000);
 
 app.get('/',(req,res)=>{
-    res.send('<div style="width:100%"><img style="width:50%;margin:auto;" src="http://backend.aquabird.pk/uploads/1694720864454P1.png" alt=""></div>')
+    res.send('<div style="width:100%"><img style="width:30%;margin:auto;" src="http://backend.aquabird.pk/uploads/1694720864454P1.png" alt=""></div>')
 })
