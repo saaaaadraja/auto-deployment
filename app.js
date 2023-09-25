a5365c6d3c9aabdfd987a1282ca58e9fb7310b03
rajasaad <rmsik92@gmail.com>
2023-09-25 19:48:19 +0500

    changes

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ const app = express();
 app.listen(3000);
 
 app.get('/',(req,res)=>{
-    res.send('<div style="width:100%;text-align: center;margin-top:5rem;"><img style="width:30%;margin:auto;" src="http://backend.aquabird.pk/uploads/1694720864454P1.png" alt=""></div>')
+    res.send(`<div style="width:100%;height:100vh;text-align: center;margin-top:5rem;background-color:black;">
+    <img style="width:30%;margin:auto;" src="http://backend.aquabird.pk/uploads/1694720864454P1.png" alt="">
+    <p style="color:white;">Accessment test...</p></div>`)
 })
